b3a5151608d0cf20621aa8c4b6a86fb74a577722
Jim Zhang <gropingzy@gmail.com>
2017-02-21 17:13:13 +0800

    list

---
+++ src/zy/collection/list/ArrayList.java
@@ -0,0 +1,22 @@
+package zy.collection.list;
+
+public class ArrayList {
+	
+	private Object[] elementData = new Object[10];
+	
+	public void add(Object obj){
+		
+	}
+	
+	public void add(int index, Object obj){
+		
+	}
+	
+	public Object get(int index){
+		return null;
+	} 
+	
+	public Object remove(int index){
+		return null;
+	}
+}

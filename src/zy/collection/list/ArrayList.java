6dd04b91852495ae07dd6d8d3d1f8b4ef1708522
Jim Zhang <gropingzy@gmail.com>
2017-02-23 17:14:40 +0800

    0223/1714

--- src/zy/collection/list/ArrayList.java
+++ src/zy/collection/list/ArrayList.java
@@ -3,48 +3,42 @@ package zy.collection.list;
 import java.util.Arrays;
 
 public class ArrayList {
-	//实际容量指针
+
 	private int size = 0;
-	//容量常量
 	private int CAPACITY = 10;
-	//list本身
 	private Object[] elementData = new Object[CAPACITY];
-	//constructor
+
 	public ArrayList(int len){
 		this.elementData = new Object[len];
 	}
 	
 	public void add(Object obj){
 		if(size < elementData.length){
-			elementData[size ++] = obj;
+			elementData[size] = obj;
 		}else{
 			int oldSize = elementData.length;
 			this.elementData = grow(CAPACITY);
-			this.elementData[oldSize ++] = obj;
+			this.elementData[oldSize] = obj;
 		}
+		size++;
+		
 	}
 	
 	public void add(int index, Object obj){
 		if(index < 0){
 			System.out.println("插入位置不合法");
 		}else{
-			//长度未超
 			if(index + 1 < elementData.length){
-				Object[] newElementData = new Object[CAPACITY];
-				for(int i=0; i<index; i++){
-					newElementData[i] = elementData[i];
-				}
-				newElementData[index] = obj;
-				for(int i=index + 1; i<elementData.length+1; i++){
-					newElementData[i] = elementData[i-1];
-				}
-				this.elementData = newElementData;
-			//长度已超，先扩容，再插入	
+				System.arraycopy(elementData, 0, this.elementData, 0, index-1);
+				System.arraycopy(elementData, index, this.elementData, index+1, elementData.length-index-1);
+				elementData[index] = obj;
 			}else{
 				Object[] newElementData = Arrays.copyOf(elementData, elementData.length + CAPACITY);
 				newElementData[index] = obj;
-				System.arraycopy(elementData, index, newElementData, index, elementData.length - index);
+				System.arraycopy(elementData, index, newElementData, index+1, elementData.length - index);
+				this.elementData = newElementData;
 			}
+			size++;
 		}
 	}
 	
--- src/zy/collection/list/ArrayListTest.java
+++ src/zy/collection/list/ArrayListTest.java
@@ -14,39 +14,39 @@ public class ArrayListTest {
 		list.add("c");
 		list.add("d");
 		
-		System.out.println("下标为3的元素为：" + list.get(3));
-		System.out.println("数组size：" + list.size());
+		//System.out.println("下标为3的元素为：" + list.get(3));
+		//System.out.println("数组size：" + list.size());
 		
-		System.out.println("删除c之前的list：");
-		for(int i=0; i<list.size(); i++){
-			System.out.println(list.get(i));
-		}
 		list.remove(2);
-		System.out.println("删除c之后的list：");
-		for(int i=0; i<list.size(); i++){
-			System.out.println(list.get(i));
-		}
+		//System.out.println("删除c之后的list：");
+		//for(int i=0; i<list.size(); i++){
+		//	System.out.print(list.get(i) + "\t");
+		//}
+		//System.out.println("");
+		//System.out.println("remove后的数组size：" + list.size());
 		
-		System.out.println("remove后的数组size：" + list.size());
-		
-		System.out.println("插入e之前的list：");
-		for(int i=0; i<list.size(); i++){
-			System.out.println(list.get(i));
-		}
 		list.add(2, "e");
 		
-		System.out.println("插入e之后的list：");
-		for(int i=0; i<list.size(); i++){
-			System.out.println(list.get(i));
-		}
-		
+		//System.out.println("插入e之后的list：");
+		//for(int i=0; i<list.size(); i++){
+		//	System.out.print(list.get(i) + "\t");
+		//}
+		//System.out.println("");
 		list.add("f");
 		list.add("g");
 		
-		//System.out.println("插入h之前的list：" + list.getElementData());
-		
-		//list.add(2, "h");
-		//System.out.println("插入h之后的list：" + list.getElementData());
+		//System.out.println("插入f、g之后的list：");
+		//for(int i=0; i<list.size(); i++){
+		//	System.out.print(list.get(i) + "\t");
+		//}
+		//System.out.println("");
+		list.add(2, "h");
+		list.remove(0);
+		//System.out.println("插入h之后的list：");
+		for(int i=0; i<list.size(); i++){
+			System.out.print(list.get(i) + "\t");
+		}
+		//System.out.println("");
 		
 		//System.out.println("下标为3的元素为：" + list.get(3));
 		
